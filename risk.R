7cd1732a71fa79f25144c9b53609325732515402
rickstra <112975250+rickstra@users.noreply.github.com>
2024-05-13 14:43:31 +0200

    Update risk.R

--- risk.R
+++ risk.R
@@ -2,53 +2,68 @@
 # install.packages("rms")
 
 PredictCORE <- function(data) {
-  
   # Load necessary rms functions
   "%ia%" <- rms::`%ia%`
   rcs <- rms::rcs
   
   # Transformation functions
-  cr <- function(x) x ^ (1 / 3)
-  risk <- function(lp) 1 - exp(-exp(lp))
+  h <- log
+  risk <- plogis
+  
+  # 
+  if (is.null(data$age)) {
+    data$age <- data$start_age
+  }
   
   # Knot locations for splines
-  kn <- list(start_age = c(20.04107, 39.35113, 46.83368, 56.37235, 79.99726), 
-             AST = c(0.10, 0.28, 0.33, 0.41, 90.90), 
-             ALT = c(0.10, 0.25, 0.35, 0.51, 96.90), 
-             GGT = c(0.10, 0.22, 0.31, 0.49, 51.31))
-  ikn <- list(start_age = c(20.04107, 46.83368, 79.99726), 
-              AST = c(0.10, 0.33, 90.90), 
-              ALT = c(0.10, 0.35, 96.90), 
-              GGT = c(0.10, 0.31, 51.31))
+  kn <- list(age = c(30.56, 39.16, 46.64, 56.18, 74.61), 
+             AST = c(0.21, 0.28, 0.33, 0.41, 0.79), 
+             ALT = c(0.15, 0.24, 0.35, 0.51, 1.33), 
+             GGT = c(0.14, 0.22, 0.31, 0.48, 1.72))
+  ikn <- list(age = c(32.90, 46.64, 64.38), 
+              AST = c(0.23, 0.33, 0.52), 
+              ALT = c(0.18, 0.35, 0.77), 
+              GGT = c(0.17, 0.31, 0.81))
   
   # Model formula
-  CORE_formula <- formula(" ~ sex + rcs(start_age, kn$start_age) +
-                        rcs(start_age, ikn$start_age) %ia% sex + 
-                        rcs(cr(GGT), cr(kn$GGT)) + 
-                        rcs(cr(GGT), cr(ikn$GGT)) %ia% sex + 
-                        rcs(cr(GGT), cr(ikn$GGT)) %ia% rcs(start_age, ikn$start_age) +
-                        rcs(cr(AST), cr(kn$AST)) + 
-                        rcs(cr(AST), cr(ikn$AST)) %ia% sex +
-                        rcs(cr(ALT), cr(kn$ALT)) + 
-                        rcs(cr(ALT), cr(ikn$ALT)) %ia% sex +
-                        rcs(cr(ALT), cr(ikn$ALT)) %ia% rcs(cr(AST), cr(ikn$AST))
+  CORE_formula <- formula(" ~ sex + rcs(age, kn$age) +
+                        rcs(age, ikn$age) %ia% sex + 
+                        rcs(h(GGT), h(kn$GGT)) + 
+                        rcs(h(AST), h(kn$AST)) + 
+                        rcs(h(ALT), h(kn$ALT)) + 
+                        rcs(h(GGT), h(ikn$GGT)) %ia% sex + 
+                        rcs(h(AST), h(ikn$AST)) %ia% sex +
+                        rcs(h(ALT), h(ikn$ALT)) %ia% sex +
+                        rcs(h(GGT), h(ikn$GGT)) %ia% rcs(age, ikn$age) +
+                        rcs(h(AST), h(ikn$AST)) %ia% rcs(age, ikn$age) +
+                        rcs(h(ALT), h(ikn$ALT)) %ia% rcs(age, ikn$age) +
+                        rcs(h(GGT), h(ikn$GGT)) %ia% rcs(h(AST), h(ikn$AST))+
+                        rcs(h(GGT), h(ikn$GGT)) %ia% rcs(h(ALT), h(ikn$ALT))+
+                        rcs(h(AST), h(ikn$AST)) %ia% rcs(h(ALT), h(ikn$ALT))
                         ") 
   # Model coefficients
-  beta <- c(alpha   = -6.516541e+0, sex      = -8.689450e-1, 
-            age     =  6.952553e-2, age2     = -6.655955e-2, 
-            age3    =  2.984620e-1, age4     = -3.315944e-1, 
-            age_sex = -8.003664e-3, age2_sex = -5.951540e-3, 
-            GGT     = -3.613434e+0, GGT2     =  1.330111e+3, 
-            GGT3    = -5.190295e+3, GGT4     =  4.691223e+3, 
-            GGT_sex =  4.054654e-1, GGT2_sex = -3.486140e+0, 
-            GGT_age =  1.931010e-2, GGT_age2 = -3.250991e-2, GGT2_age = -4.515743e-2, 
-            AST     = -2.159130e+0, AST2     = -3.295898e+1, 
-            AST3    =  8.942042e+3, AST4     = -1.630740e+4, 
-            AST_sex =  6.370176e-1, AST2_sex =  2.425573e+0, 
-            ALT     = -4.434939e+0, ALT2     =  2.157671e+2, 
-            ALT3    = -1.820423e+2, ALT4     = -2.838977e+2, 
-            ALT_sex =  9.013652e-1, ALT2_sex = -5.125481e+1, 
-            ALT_AST =  3.678255e+0, ALT_AST2 =  1.035145e+2, AST2_AST = -9.628953e+1
+  beta <- c(alpha    = -9.254482879, sex      =  3.667106987, 
+            age      =  0.057032410, age2     = -0.204268545, 
+            age3     =  0.406601401, age4     = -0.326945974, 
+            age_sex  = -0.029801996, age2_sex =  0.013586036, 
+            GGT      =  1.630793452, GGT2     =  16.71805261, 
+            GGT3     = -54.37998654, GGT4     =  49.01438678,
+            AST      =  5.511944025, AST2     = -8.585417058, 
+            AST3     =  49.33950323, AST4     = -65.23172726, 
+            ALT      = -2.317930678, ALT2     = -1.523886029, 
+            ALT3     =  1.805714672, ALT4     =  4.053019631, 
+            GGT_sex  =  0.619220765, GGT2_sex = -0.745079371,
+            AST_sex  =  0.577803458, AST2_sex = -0.266060892, 
+            ALT_sex  =  0.351862597, ALT2_sex = -0.546217603, 
+            GGT_age  = -0.042883894, GGT_age2 =  0.016134286, 
+            GGT2_age =  0.017862019, AST_age  = -0.008686066, 
+            AST_age2 = -0.042139306, AST2_age =  0.032095785,
+            ALT_age  =  0.019174037, ALT_age2 =  0.032095785, 
+            ALT2_age = -0.016519747, GGT_AST  =  2.066139042, 
+            GGT_AST2 =  0.571482852, GGT2_AST = -2.620549353,
+            GGT_ALT  = -1.408748611, GGT_ALT2 = -0.671668287, 
+            GGT2_ALT =  1.733937890, AST_ALT  =  0.943178392, 
+            AST_ALT2 = -0.797298460, AST2_ALT = -0.028686747
   )
   # Linear predictor
   X <- model.matrix(CORE_formula, data)
@@ -58,47 +73,97 @@ PredictCORE <- function(data) {
   risk(lp)
 }
 
-# Check that the prediction function works
 test_data <- data.frame(Id = 1:5, 
-                        start_age = c(33.33, 64.82, 48.65, 42.95, 49.15), 
+                        age = c(33.33, 64.82, 48.65, 42.95, 49.15), 
                         sex = c(0, 0, 0, 1, 1), 
-                        AST = c(0.29, 0.33, 0.22, 0.57, 0.34),
-                        ALT = c(0.27, 0.22, 0.33, 0.76, 0.46),
-                        GGT = c(0.19, 0.38, 0.67, 0.30, 0.18))
-PredictCORE(test_data)
+                        AST = c(0.29, 0.33, 0.22, 0.57, 50),
+                        ALT = c(0.27, 0.22, 0.33, 10, 0.46),
+                        GGT = c(0.19, 0.38, 50, 0.30, 0.18))
 
-# Read in your data
+# read in your data here (using e.g. read.table, load, haven::read_dta, ...)
 # data <-
 
 # Generate predictions
 p <- PredictCORE(data)
+data$p <- p
 
-# Calculate AUC
+# Calculate AUC #---------------------------------------------------------------
+
 # Requires the timeROC package
 # install.packages("timeROC")
 auc <- with(data,
-         timeROC::timeROC(time, event, p, cause = 1, times = 1:10, 
-                          ROC = FALSE)$AUC_2
-       )
+  timeROC::timeROC(time, event, p, cause = 1, times = 1:10, ROC = FALSE)$AUC_2
+)
 auc
-# Observed:Expected ratio
-# Requires cmprsk package
-# install.packages("cmprsk")
-cinc <- with(data, as.data.frame(cmprsk::cuminc(time, event)[[1]]))
-observed <- cinc[which(cinc$time > 10)[1] - 1, ]
-expected <- mean(p)
 
-oe <- exp(log(observed$est) - log(expected) + 
-      c(-1, 0, 1) * 1.96 * sqrt(var(p) / length(p) / expected ^ 2 + 
+# Observed:Expected ratio #-----------------------------------------------------
+# Requires cmprisk package
+# install.packages("cmprisk")
+cinc <- with(data, as.data.frame(cmprisk::cuminc(time, event)[[1]]))
+observed <- cinc[which(cinc$time > 10)[1] - 1, ]
+expected <- mean(data$p)
+
+oe <- exp(log(observed$est) - log(expected) +
+      c(-1, 0, 1) * 1.96 * sqrt(var(p) / length(p) / expected ^ 2 +
                                observed$var / observed$est ^ 2))
 oe
 
+# Calibration curves #----------------------------------------------------------
+# Prepare data for calibration curves
 
+# install.packages("tidyverse")
+library(tidyverse)
+{
+  cal_df <- select(data, time, event, p) %>% 
+    mutate(event = ifelse(time > 10, 0, event), 
+           time = pmin(time, 10), 
+           p = pmin(p, 1)
+    ) %>% arrange(p)
+  
+  cal_df$cumev <- cumsum(cal_df$event==1)
+  m <- pmin(pmax(floor(max(cal_df$cumev) / 15), 20), 50)
+  cal_df$set <- cal_df$cumev %/% m
+  
+  cdf <- cal_df %>% group_by(set) %>% summarise(n = length(p),
+                                                mean_pred = mean(p),
+                                                lower_pred = min(p), 
+                                                upper_pred = max(p))
+  cinc <- with(cal_df, cmprsk::cuminc(time, event, set))
+  cinc$Tests <- NULL
+  
+  cinc <- sapply(1:(length(cinc)/2), function(i) {
+    d <- as.data.frame(cinc[[i]])
+    d <- unlist(d[nrow(d), ])
+  }) %>% t %>% as.data.frame
+  
+  cdf$obs <- cinc$est
+  cdf$lower <- pmax(cinc$est - 1.96 * sqrt(cinc$var), 0)
+  cdf$upper <- pmin(cinc$est + 1.96 * sqrt(cinc$var), 1)
+}
 
+# Plot
+cal_plot <- ggplot(cdf, aes(mean_pred, obs)) + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = lower, ymax = upper, width = 0)) + 
+  geom_segment(aes(x = lower_pred, xend = upper_pred, y = lower)) + 
+  geom_segment(aes(x = lower_pred, xend = upper_pred, y = upper)) + 
+  geom_smooth() +
+  xlim(0, 1) + ylim(0, 1) + 
+  geom_abline(intercept = 0, slope = 1, linetype = 3)
 
+# Zoom in on p = 0-0.2 
+p_max <- 0.2 
+cal_plot2 <- ggplot(cdf, aes(mean_pred, obs)) + geom_point() + 
+  geom_errorbar(aes(ymin = lower, ymax = pmin(upper, p_max), width = 0)) + 
+  geom_segment(aes(x = lower_pred, xend = pmin(upper_pred, p_max), y = lower)) + 
+  geom_segment(aes(x = lower_pred, xend = pmin(upper_pred, p_max), y = upper)) + 
+  geom_smooth() +
+  xlim(0, p_max) + ylim(0, p_max) + 
+  geom_abline(intercept = 0, slope = 1, linetype = 3) 
 
-
-
-
-
+# All results collected #-------------------------------------------------------
+auc
+oe
+cal_plot
+cal_plot2
 
