bbd07a40ad7dee0f61a13829a8dd2cb531f0f4d7
rickstra <112975250+rickstra@users.noreply.github.com>
2024-06-02 16:41:26 +0200

    Update app.R

--- calculator/app.R
+++ calculator/app.R
@@ -39,7 +39,7 @@ ui <- page_sidebar(
       )
     ),
     card(
-      card_header("GGT (Gamma-Glutamate Transferase)"),
+      card_header("GGT (Gamma-Glutamyl Transferase)"),
       radioButtons(
         "ggt_unit", NULL,
         choices = list("microkat/L" = 1, "U/L" = 1/60),
@@ -75,13 +75,13 @@ ui <- page_sidebar(
   ),
   card(
     #textOutput("risk")),
-    value_box("The 10 year risk is:", textOutput("risk"),
-              showcase = icon("heart-pulse"),
-              theme = "green"
-              )#,
+    value_box("The estimated 10-year risk of liver cirrhosis or complications thereof is:", textOutput("risk"),
+              showcase = icon("user-doctor"),
+              theme = value_box_theme(bg = "#4F0433")
+              ),
     #valueBoxOutput("vbox")
-    ),
-card(
+    #),
+#card(
     verbatimTextOutput("messages")) 
 )
 
@@ -93,18 +93,19 @@ server <- function(input, output) {
       y <- gsub(",", ".", x, fixed = TRUE)
       y <- as.numeric(y)
     }
-    if (is.na(y) | y < 0) {
+    if (is.na(y) | y <= 0) {
       return(paste0(var, " has an invalid format."))
     } else {
       return(y)
     }
   }
-  ValueWarning <- function(x, l = 0, u = Inf, var) {
-    unit <- ifelse(var == "Age", " years.", " microkat/L.") 
+  ValueWarning <- function(x, l = 0, u = Inf, var, unit = 1) {
+    end <- ifelse(var == "Age", " years.", 
+                  ifelse(unit == 1, " microkat/L.", " U/L."))
     if (x < l) {
-      paste0(var, " is extremely low. The estimate might be unreliable below ", l, unit)
+      paste0(var, " is extremely low. The estimate might be unreliable below ", l, end)
     } else if (x > u) {
-      paste0(var, " is extremely high. The estimate might be unreliable above ", u, unit)
+      paste0(var, " is extremely high. The estimate might be unreliable above ", u, end)
     } else {
       NULL
     }
@@ -118,6 +119,10 @@ server <- function(input, output) {
     ast <- NumberCheck(input$ast, "AST")
     alt <- NumberCheck(input$alt, "ALT")
     
+    ggt_unit <- as.numeric(input$ggt_unit)
+    ast_unit <- as.numeric(input$ast_unit)
+    alt_unit <- as.numeric(input$alt_unit)
+    
     varlist <- list(age, ggt, ast, alt)
     
     valid <- sapply(varlist, is.numeric)
@@ -127,17 +132,17 @@ server <- function(input, output) {
     } else {
       
       val_args <- list(list(age, 20, 80, "Age"), 
-           list(ggt, 0, 25, "GGT"), 
-           list(ast, 0, 12, "AST"), 
-           list(alt, 0, 20, "ALT"))
+           list(ggt, 0, round(25 / ggt_unit), "GGT", ggt_unit), 
+           list(ast, 0, round(12 / ast_unit), "AST", ast_unit), 
+           list(alt, 0, round(20 / alt_unit), "ALT", alt_unit))
       output$messages <- renderText({paste0(unlist(sapply(val_args, 
                                                           function(lst) do.call(ValueWarning, lst))), 
                                             collapse = "\n")})
       d <- data.frame(age = age,
                       sex = factor(input$sex, 0:1),
-                      GGT = ggt * as.numeric(input$ggt_unit),
-                      AST = ast * as.numeric(input$ast_unit),
-                      ALT = alt * as.numeric(input$alt_unit)
+                      GGT = ggt * ggt_unit,
+                      AST = ast * ast_unit,
+                      ALT = alt * alt_unit
       )
       # f <- ifelse(input$risk_group == 1, 
       #             PredictCORE_full, 
