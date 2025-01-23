dc04cf37fb67f2b3eeeb0b46ea7bcb3ad5fb5f8d
rickstra <112975250+rickstra@users.noreply.github.com>
2025-01-23 10:50:03 +0100

    Update CORE_all.R

--- calculator/CORE_all.R
+++ calculator/CORE_all.R
@@ -13,59 +13,64 @@ PredictCORE_full <- function(data) {
   }
   
   # Knot locations for splines
-  kn <- list(age = c(30.56, 39.16, 46.64, 56.18, 74.61), 
-             AST = c(0.21, 0.28, 0.33, 0.41, 0.79), 
-             ALT = c(0.15, 0.24, 0.35, 0.51, 1.33), 
-             GGT = c(0.14, 0.22, 0.31, 0.48, 1.72))
-  ikn <- list(age = c(32.90, 46.64, 64.38), 
-              AST = c(0.23, 0.33, 0.52), 
-              ALT = c(0.18, 0.35, 0.77), 
-              GGT = c(0.17, 0.31, 0.81))
+  
+  
+  kn <- list(age = c(30.65, 39.27, 46.68, 56.16, 70.52), 
+             AST = c(0.21, 0.30, 0.37, 0.63, 1.81), 
+             ALT = c(0.15, 0.28, 0.43, 1.01, 2.66), 
+             GGT = c(0.14, 0.25, 0.39, 1.18, 5.0))
   
   # Model formula
   CORE_formula <- formula(" ~ sex + rcs(age, kn$age) +
-                        rcs(age, ikn$age) %ia% sex + 
+                        rcs(age, kn$age) %ia% sex + 
                         rcs(h(GGT), h(kn$GGT)) + 
                         rcs(h(AST), h(kn$AST)) + 
                         rcs(h(ALT), h(kn$ALT)) + 
-                        rcs(h(GGT), h(ikn$GGT)) %ia% sex + 
-                        rcs(h(AST), h(ikn$AST)) %ia% sex +
-                        rcs(h(ALT), h(ikn$ALT)) %ia% sex +
-                        rcs(h(GGT), h(ikn$GGT)) %ia% rcs(age, ikn$age) +
-                        rcs(h(AST), h(ikn$AST)) %ia% rcs(age, ikn$age) +
-                        rcs(h(ALT), h(ikn$ALT)) %ia% rcs(age, ikn$age) +
-                        rcs(h(GGT), h(ikn$GGT)) %ia% rcs(h(AST), h(ikn$AST))+
-                        rcs(h(GGT), h(ikn$GGT)) %ia% rcs(h(ALT), h(ikn$ALT))+
-                        rcs(h(AST), h(ikn$AST)) %ia% rcs(h(ALT), h(ikn$ALT))
+                        rcs(h(GGT), h(kn$GGT)) %ia% sex + 
+                        rcs(h(AST), h(kn$AST)) %ia% sex +
+                        rcs(h(ALT), h(kn$ALT)) %ia% sex +
+                        rcs(h(GGT), h(kn$GGT)) %ia% rcs(age, kn$age) +
+                        rcs(h(GGT), h(kn$GGT)) %ia% rcs(h(AST), h(kn$AST))+
+                        rcs(h(GGT), h(kn$GGT)) %ia% rcs(h(ALT), h(kn$ALT))+
+                        rcs(h(AST), h(kn$AST)) %ia% rcs(h(ALT), h(kn$ALT))
                         ") 
-  # Model coefficients
-  beta <- c(alpha    = -9.254482879, sex      =  3.667106987, 
-            age      =  0.057032410, age2     = -0.204268545, 
-            age3     =  0.406601401, age4     = -0.326945974, 
-            age_sex  = -0.029801996, age2_sex =  0.013586036, 
-            GGT      =  1.630793452, GGT2     =  16.71805261, 
-            GGT3     = -54.37998654, GGT4     =  49.01438678,
-            AST      =  5.511944025, AST2     = -8.585417058, 
-            AST3     =  49.33950323, AST4     = -65.23172726, 
-            ALT      = -2.317930678, ALT2     = -1.523886029, 
-            ALT3     =  1.805714672, ALT4     =  4.053019631, 
-            GGT_sex  =  0.619220765, GGT2_sex = -0.745079371,
-            AST_sex  =  0.577803458, AST2_sex = -0.266060892, 
-            ALT_sex  =  0.351862597, ALT2_sex = -0.546217603, 
-            GGT_age  = -0.042883894, GGT_age2 =  0.016134286, 
-            GGT2_age =  0.017862019, AST_age  = -0.008686066, 
-            AST_age2 = -0.042139306, AST2_age =  0.032095785,
-            ALT_age  =  0.019174037, ALT_age2 =  0.032095785, 
-            ALT2_age = -0.016519747, GGT_AST  =  2.066139042, 
-            GGT_AST2 =  0.571482852, GGT2_AST = -2.620549353,
-            GGT_ALT  = -1.408748611, GGT_ALT2 = -0.671668287, 
-            GGT2_ALT =  1.733937890, AST_ALT  =  0.943178392, 
-            AST_ALT2 = -0.797298460, AST2_ALT = -0.028686747
+  # Coefficients
+  beta <- c(alpha    = -13.00009578, sex      =   0.60980873,
+            age      =   0.11304760, age2     =  -0.26559739, 
+            age3     =   0.53007135, age4     =  -0.23699264, 
+            age_sex  =  -0.04002686, age2_sex =   0.23812831, 
+            age3_sex =  -0.37176718, age4_sex =  -0.14079736, 
+            GGT      =  -0.96652829, GGT2     =  26.11194089, 
+            GGT3     = -79.41819012, GGT4     =  62.58544918,
+            AST      =  -0.62542781, AST2     =   4.20000076, 
+            AST3     =  16.03675909, AST4     = -36.41540273, 
+            ALT      =  -0.52588932, ALT2     =   1.30767217, 
+            ALT3     =  -4.81015945, ALT4     =   1.95395416, 
+            GGT_sex  =  -0.26228717, GGT2_sex =   0.72371978,
+            GGT3_sex =   1.33605173, GGT4_sex =  -3.76002473,
+            AST_sex  =  -0.24704669, AST2_sex =  -5.62259586, 
+            AST3_sex =  26.06519108, AST4_sex = -25.30405136, 
+            ALT_sex  =  -0.03140536, ALT2_sex =  -1.03656480, 
+            ALT3_sex =   5.35217631, ALT4_sex =  -5.27551838, 
+            GGT_age  =   0.01526520, GGT_age2 =  -0.07484819, 
+            GGT_age3 =   0.19591789, GGT_age4 =  -0.17511797, 
+            GGT2_age =  -0.23356007, GGT3_age =   0.81073184, 
+            GGT4_age =  -0.71781504, GGT_AST  =  -0.86358788, 
+            GGT_AST2 =   2.61912495, GGT_AST3 =   4.39796651, 
+            GGT_AST4 = -13.08487962, GGT2_AST =   7.73735030,
+            GGT3_AST = -21.15764757, GGT4_AST =  14.02740518, 
+            GGT_ALT  =   0.01375014, GGT_ALT2 =  -0.82961745, 
+            GGT_ALT3 =  -3.16116607, GGT_ALT4 =   7.65893291, 
+            GGT2_ALT =  -1.72353722, GGT3_ALT =   5.50934118, 
+            GGT4_ALT =  -3.65492090, AST_ALT  =  -0.03154256, 
+            AST_ALT2 =   0.34739590, AST_ALT3 =   0.26474348, 
+            AST_ALT4 =  -4.60623685, AST2_ALT =  -6.32755773, 
+            AST3_ALT =  40.63303767, AST4_ALT = -43.48380371 
   )
   # Linear predictor
-  X <- model.matrix(CORE_formula, data)
+  X <- model.matrix.lm(CORE_formula, data, na.action = "na.pass")
   lp <- X %*% beta
   
   # Risk
-  risk(lp)
+  c(risk(lp))
 }
